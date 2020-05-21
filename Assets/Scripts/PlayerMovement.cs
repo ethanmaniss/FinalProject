d16c05bc6092eef36a2b01cad1d33493a97c397d
ethanmaniss <54688877+ethanmaniss@users.noreply.github.com>
2020-05-20 18:49:56 -0700

    Commit number 1
    contains base of the project, most assets/etc.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 25f;
+
+    public AudioClip jumpClip;
+
+    float horizontalMove = 0f;
+    bool jumpFlag = false;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(jumpFlag)
+        {
+            animator.SetBool("IsJumping", true);
+            jumpFlag = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(animator.GetBool("IsJumping") == false)
+            {
+                AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+                jump = true;
+                animator.SetBool("IsJumping", true);
+            }
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if(jump)
+        {
+            jumpFlag = true;
+        }
+    }
+}
